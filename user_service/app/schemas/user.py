55d72ca839ccdec7bde87d6060e2c5bcd75421c2
mkonchev <maksim1901m@gmail.com>
2026-01-30 20:46:31 +0300

    add password validation

--- user_service/app/schemas/user.py
+++ user_service/app/schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, field_validator
 from typing import Optional
 from datetime import datetime
 
@@ -12,6 +12,20 @@ class UserBase(BaseModel):
 
 class UserCreate(UserBase):
     password: str = Field(description="Password")
+
+    @field_validator('password')
+    def validate_password(cls, v):
+        if len(v) < 8:
+            raise ValueError("Password must be at least 8 characters")
+        if not any(c.isdigit() for c in v):
+            raise ValueError("Password must contain at least one digit")
+        if v.islower():
+            raise ValueError("Password must contain one uppercase sym")
+        if not set('!@#$%&*').intersection(v):
+            raise ValueError(
+                "Password must contain at least one symbol like(! @ # $ % & *)"
+            )
+        return v
 
 
 class UserResponse(UserBase):
